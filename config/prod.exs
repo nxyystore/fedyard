31a76a80118e37dd10ff5a732f75ec815fcc798c
nxyy <nxyy@nxyy.store>
2025-05-30 10:37:34 -0400

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,7 @@ config :lanyard,
   discord_spotify_activity_id: "spotify:1",
   command_prefix: System.get_env("COMMAND_PREFIX") || ".",
   bot_presence: System.get_env("BOT_PRESENCE") || "swappi.ng",
-  bot_presence_type: String.to_integer(System.get_env("BOT_PRESENCE_TYPE") || "4"),
+  bot_presence_type: String.to_integer(System.get_env("BOT_PRESENCE_TYPE") || "3"),
   bot_token: System.get_env("BOT_TOKEN"),
   redis_uri:
     System.get_env("REDIS_DSN") || System.get_env("REDIS_URI") || System.get_env("REDIS_URL")
