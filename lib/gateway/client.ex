77375e94478c5440985ce12d183999de715994d4
nxyy <nxyy@nxyy.store>
2025-06-15 21:28:21 -0400

    Update client.ex

--- lib/gateway/client.ex
+++ lib/gateway/client.ex
@@ -285,7 +285,7 @@ defmodule Lanyard.Gateway.Client do
           "name" => Application.get_env(:lanyard, :bot_presence),
           "type" => Application.get_env(:lanyard, :bot_presence_type)
         },
-        "status" => Application.get_env(:lanyard, :bot_status)
+        "status" => "idle"
       },
       "compress" => false,
       "large_threshold" => 250,
