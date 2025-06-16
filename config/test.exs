7c799760cfe1a42475a69b6a9f7930474634fad9
nxyy <nxyy@nxyy.store>
2025-06-15 21:12:54 -0400

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,10 @@
 import Config
 
 config :lanyard,
-  http_port: String.to_integer(System.get_env("PORT") || "4001"),
+  http_port: String.to_integer(System.get_env("PORT") || "5001"),
   discord_spotify_activity_id: "spotify:1",
   command_prefix: System.get_env("COMMAND_PREFIX") || ".",
-  bot_presence: System.get_env("BOT_PRESENCE") || "you <3",
+  bot_presence: System.get_env("BOT_PRESENCE") || "swappi.ng",
   bot_presence_type: String.to_integer(System.get_env("BOT_PRESENCE_TYPE") || "3"),
   bot_token: System.get_env("BOT_TOKEN"),
   redis_uri: "redis://localhost"
