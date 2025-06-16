d758ea848738a95f56334e9c64d2bca3087ea1dd
nxyy <nxyy@nxyy.store>
2025-06-15 21:19:15 -0400

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ config :lanyard,
   command_prefix: System.get_env("COMMAND_PREFIX") || ".",
   bot_presence: System.get_env("BOT_PRESENCE") || "swappi.ng",
   bot_presence_type: String.to_integer(System.get_env("BOT_PRESENCE_TYPE") || "3"),
+  bot_status: String.get_env("BOT_STATUS") || "online",
   bot_token: System.get_env("BOT_TOKEN"),
   redis_uri:
     System.get_env("REDIS_DSN") || System.get_env("REDIS_URI") || System.get_env("REDIS_URL")
